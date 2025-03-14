9b96fdb891ffd2a014faa03e1f81417dca177237
MarcoTDeveloper <marcosdevprofessional@gmail.com>
2025-03-14 02:37:29 -0300

    feat: add axios and tanstack for working with APIs and cache

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,13 @@
+import { queryClient } from '@src/shared/services/tanstack-config'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 
 import { router } from './routes'
 
 export function Providers() {
-	return <RouterProvider router={router} />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	)
 }
