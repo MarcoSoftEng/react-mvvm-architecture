2a7d860e417efb9c8457fb5663f81071f9a89c38
MarcoTDeveloper <marcosdevprofessional@gmail.com>
2025-03-14 02:42:29 -0300

    fix: add hook to apply theme in providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,3 +1,4 @@
+import { useApplyTheme } from '@src/shared/hooks/use-apply-theme'
 import { queryClient } from '@src/shared/services/tanstack-config'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
@@ -5,6 +6,8 @@ import { RouterProvider } from 'react-router'
 import { router } from './routes'
 
 export function Providers() {
+	useApplyTheme()
+
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
